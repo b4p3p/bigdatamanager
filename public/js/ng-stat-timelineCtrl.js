b8f33cfa05223652de04a342ceba9df587ff83cc
shiner88 <annaritafierro@gmail.com>
2015-10-17 13:17:03 +0200

    timeline

--- public/js/ng-stat-timelineCtrl.js
+++ public/js/ng-stat-timelineCtrl.js
@@ -2,6 +2,10 @@
 
 ngApp.controller('ngStatTimeLineCtrl', ['$scope', function($scope) {
 
+    $scope.hasNotData = function(){
+
+    };
+
     function getDateOfWeek(w, y) {
         var d = (1 + (w - 1) * 7); // 1st of January + 7 days for each week
         return new Date(y, 0, d);
